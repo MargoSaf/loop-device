9a6685ad0548e70501c490ddc3159d76483155e0
Margo <margosafaryan@gmail.com>
2024-03-18 23:28:07 +0400

    Code improvement

--- loop_dev.c
+++ loop_dev.c
@@ -11,7 +11,7 @@
 
 #define DEVICE_NAME     "loop_dev"
 #define OUTPUT_FILE     "/tmp/output"
-#define ROW_LEN         48
+#define ROW_LEN         41
 #define BYTES_PER_ROW   16
 #define HEAD_LEN        7
 
@@ -37,8 +37,23 @@ static int loop_dev_release(struct inode *inode, struct file *file)
 }
 
 // Function to open the file and return the offset
-static loff_t open_file(bool is_file_new, struct file **output_file) 
+static loff_t open_file(struct file *input_file, struct file **output_file) 
 {
+    bool is_file_new = true;
+
+    // Check if the current file is the same as the last one
+    if (last_file == input_file)
+    {
+        is_file_new = false;
+    }
+    else
+    {
+        // Reset last file length when switching to a new file
+        last_file_len = 0;
+    }
+
+    last_file = input_file;
+    
     loff_t offset = 0;
     loff_t file_len = 0;
 
@@ -84,30 +99,14 @@ static ssize_t write_file(struct file *output_file, unsigned char *hex_buffer, s
 // Function to handle write operations to the loop device
 static ssize_t loop_dev_write(struct file *file, const char *buffer, size_t length, loff_t *offset)
 {
-    bool new_file = true;
-
-    // Check if the current file is the same as the last one
-    if (last_file == file)
-    {
-        new_file = false;
-    }
-    else
-    {
-        // Reset last file length when switching to a new file
-        last_file_len = 0;
-    }
-
-    last_file = file;
-    
     struct file *output_file;
-    loff_t file_offset = open_file(new_file, &output_file);
+    loff_t file_offset = open_file(file, &output_file);
     
-    unsigned char row[ROW_LEN - HEAD_LEN];
-    unsigned char last_written_row[ROW_LEN - HEAD_LEN];
-
+    unsigned char row[ROW_LEN];
+    unsigned char last_written_row[ROW_LEN];
 
     size_t total_bytes_read = 0;
-    bool is_exist = false;
+    bool is_row_repeating = false;
 
     while (total_bytes_read < length)
     {
@@ -140,16 +139,16 @@ static ssize_t loop_dev_write(struct file *file, const char *buffer, size_t leng
         }
 
         // Check if the current row is different from the last written row
-        if (total_bytes_read == 0 || memcmp(last_written_row, row, ROW_LEN - HEAD_LEN) != 0)
+        if (total_bytes_read == 0 || memcmp(last_written_row, row, ROW_LEN) != 0)
         {
-            unsigned char temp[ROW_LEN];
-            if (is_exist)
+            unsigned char temp[ROW_LEN + HEAD_LEN];
+            if (is_row_repeating)
             {
                 
                 sprintf(temp, "*\n");
                 int ret = write_file(output_file, temp, 2 , file_offset);
                 file_offset+=2;
-                is_exist = false;
+                is_row_repeating = false;
             }
             
             // Write the offset of the current row
@@ -158,19 +157,19 @@ static ssize_t loop_dev_write(struct file *file, const char *buffer, size_t leng
             // Copy the current row to the hex buffer
             memcpy(temp + HEAD_LEN, row, row_current_len);
 
-            memcpy(last_written_row, row, ROW_LEN - HEAD_LEN);
+            memcpy(last_written_row, row, ROW_LEN);
 
             // Add spaces to fill up the row length
-            int space_count = ROW_LEN - row_current_len - 1 - HEAD_LEN;
+            int space_count = ROW_LEN - row_current_len - 1;
             if (space_count > 0)
             {
                 sprintf(temp + HEAD_LEN + row_current_len, "%*s", space_count, " ");
             }
 
             // Mark the end of the current row
-            sprintf(temp + ROW_LEN -1, "\n");
-            int ret = write_file(output_file, temp, ROW_LEN , file_offset);
-            file_offset+=ROW_LEN;
+            sprintf(temp + ROW_LEN + HEAD_LEN - 1, "\n");
+            int ret = write_file(output_file, temp, ROW_LEN + HEAD_LEN, file_offset);
+            file_offset += ROW_LEN + HEAD_LEN;
             if (ret < 0)
             {
               printk(KERN_ALERT "Failed to write data to output file\n");
@@ -178,12 +177,12 @@ static ssize_t loop_dev_write(struct file *file, const char *buffer, size_t leng
         }
         else
         {
-            is_exist = true;
+            is_row_repeating = true;
         }
 
         total_bytes_read += i;
     }
-    unsigned char temp[ROW_LEN];
+    unsigned char temp[HEAD_LEN + 1];
     // Write the offset of the end of the data
     sprintf(temp, "%07zx", total_bytes_read + last_file_len);
     //file_offset += HEAD_LEN;
