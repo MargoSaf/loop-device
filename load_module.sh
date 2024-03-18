04437c28babac90532cd5694bfc602168aaec91d
Margo Safaryan <margo.safaryan@helpsystems.com>
2024-03-18 23:59:09 +0400

    Code improvement

--- load_module.sh
+++ load_module.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Remove the kernel module
-# This line can fail if the kernel module is not loaded yet, it will not impact on result
+# This line can fail if the kernel module is not loaded yet, it will not impact the final result
 rmmod loop_dev.ko
 
 # Insert the kernel module
 insmod loop_dev.ko
 
 # Remove the device node
-# This line can fail if device node is not created, it will not impact on result
+# This line can fail if the device node is not created, it will not impact the final result
 rm /dev/loop_dev
 
 # Create a new device node with major number 240 and minor number 0
-# The Major number could be differente for the system, if this line fail, need to check the major number
+# The Major number could be different for the system, if this line fails, need to check the major number
 mknod /dev/loop_dev c 240 0
 
-# major number could be found in kernel messgaes, run following command:  
+# major number could be found in kernel messages, run the following command:  
 # cmd > dmesg
 # last log line should be "Registered a major number N". The N is the device number.
 # To compleate device creatone replace 240 with N and run mknod command again
