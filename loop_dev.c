6921e94955f2fa90782f7940a0b60c67e0015df6
Margo <margosafaryan@gmail.com>
2024-03-18 19:17:59 +0400

    Added larg files handling

--- loop_dev.c
+++ loop_dev.c
@@ -4,57 +4,84 @@
 #include <linux/uaccess.h>
 #include <linux/hex.h>
 #include <linux/slab.h> 
+#include <linux/file.h>
+#include <linux/buffer_head.h>
 
-#define DEVICE_NAME     "loop_device"
+#define DEVICE_NAME     "loop_dev"
 #define OUTPUT_FILE     "/tmp/output"
 #define ROW_LEN	        48
 #define BYTES_PER_ROW   16
 #define HEAD_LEN        7
 
 static int major_num;
+struct file *last_file;
+static size_t last_file_len = 0;
 
-static int chardev_open(struct inode *inode, struct file *file)
+static int loop_dev_open(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "chardev_example: Device opened\n");
+    printk(KERN_INFO "loop_dev: Device opened\n");
     return 0;
 }
 
-static int chardev_release(struct inode *inode, struct file *file)
+static int loop_dev_release(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "chardev_example: Device closed\n");
+    printk(KERN_INFO "loop_dev: Device closed\n");
     return 0;
 }
 
-static ssize_t chardev_write_in_file(unsigned char *hex_buffer,size_t length, loff_t *offset)
+static ssize_t write_in_file(unsigned char *hex_buffer, size_t length, bool is_file_new) 
 {
     struct file *output_file = NULL;
     int ret = 0;
-    
-    // Open the output file with the O_TRUNC flag
-    output_file = filp_open(OUTPUT_FILE, O_WRONLY | O_CREAT | O_TRUNC | O_LARGEFILE, 0644);
+    loff_t offset = 0;
+    loff_t file_len = 0;
+    // Open the output file with appropriate flags based on the offset
+    if (is_file_new)
+        output_file = filp_open(OUTPUT_FILE, O_WRONLY | O_CREAT | O_TRUNC | O_LARGEFILE, 0666);
+    else 
+    {
+        output_file = filp_open(OUTPUT_FILE, O_WRONLY | O_LARGEFILE, 0666);
+        if (!IS_ERR(output_file)) 
+        {
+            file_len = vfs_llseek(output_file, 0, SEEK_END);
+            offset = (file_len > 8) ? file_len - 8 : 0;
+        }
+    }
+
     if (IS_ERR(output_file)) 
     {
         printk(KERN_ALERT "Failed to open output file\n");
         return PTR_ERR(output_file);
     }
-    
-    ret = kernel_write(output_file, hex_buffer, length, 0);
-    
+
+    // Write data to the file at the specified offset
+    ret = kernel_write(output_file, hex_buffer, length, &offset);
+
     if (ret < 0) 
     {
         printk(KERN_ALERT "Failed to write data to output file\n");
-        filp_close(output_file, NULL); 
+        filp_close(output_file, NULL);
         return ret;
     }
-    
+
     // Close the output file
     filp_close(output_file, NULL);
     return ret;
 }
 
 
-static ssize_t chardev_write(struct file *file, const char *buffer, size_t length, loff_t *offset)
+static ssize_t loop_dev_write(struct file *file, const char *buffer, size_t length, loff_t *offset)
 {
+    bool new_file = true;
+    if(last_file == file)
+    {
+       new_file = false;
+
+    } else last_file_len = 0;
+    
+    last_file = file;
+
+
     unsigned char *hex_buffer = kmalloc(( length / BYTES_PER_ROW + 2) * ROW_LEN, GFP_KERNEL); 
     unsigned char row[ROW_LEN - HEAD_LEN];
     unsigned char last_writen_row[ROW_LEN - HEAD_LEN];
@@ -107,7 +134,7 @@ static ssize_t chardev_write(struct file *file, const char *buffer, size_t lengt
                 is_exist = false;
             }
             
-            sprintf(hex_buffer + hex_buffer_index, "%07zx", total_bytes_read);
+            sprintf(hex_buffer + hex_buffer_index, "%07zx", total_bytes_read + last_file_len);
             hex_buffer_index+=7;
 	    memcpy(hex_buffer + hex_buffer_index, row, row_current_len);
             hex_buffer_index += row_current_len;
@@ -133,27 +160,30 @@ static ssize_t chardev_write(struct file *file, const char *buffer, size_t lengt
 
         
 
-    sprintf(hex_buffer + hex_buffer_index, "%07zx", total_bytes_read);
+    sprintf(hex_buffer + hex_buffer_index, "%07zx", total_bytes_read + last_file_len);
     hex_buffer_index += HEAD_LEN;
     
     sprintf(hex_buffer + hex_buffer_index, "\n");
     hex_buffer_index++;
-    
-    int ret = chardev_write_in_file(hex_buffer, hex_buffer_index, offset);
-   
+       last_file_len += total_bytes_read;
+    int ret = write_in_file(hex_buffer, hex_buffer_index, new_file);
+    if (ret < 0) 
+    {
+        printk(KERN_ALERT "Failed to write data to output file\n");
+    }
     kfree(hex_buffer); 
-  //  kfree(row); 
+ 
     return length;
 }
 
 static struct file_operations fops = {
-    .open = chardev_open,
-    .release = chardev_release,
+    .open = loop_dev_open,
+    .release = loop_dev_release,
     .read = NULL,
-    .write = chardev_write,
+    .write = loop_dev_write,
 };
 
-static int __init chardev_init(void)
+static int __init loop_dev_init(void)
 {
     major_num = register_chrdev(0, DEVICE_NAME, &fops);
     if (major_num < 0) {
@@ -164,14 +194,14 @@ static int __init chardev_init(void)
     return 0;
 }
 
-static void __exit chardev_exit(void)
+static void __exit loop_dev_exit(void)
 {
     unregister_chrdev(major_num, DEVICE_NAME);
     printk(KERN_INFO "Unregistered the major number %d\n", major_num);
 }
 
-module_init(chardev_init);
-module_exit(chardev_exit);
+module_init(loop_dev_init);
+module_exit(loop_dev_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Margo");
